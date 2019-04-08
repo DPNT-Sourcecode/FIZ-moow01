a0b1e1c17ba5a11ba7f6515f8aa40f3968b5d5c1
sbx_user1051 <sbx_user1051@169.254.50.93>
2019-04-08 15:38:22 +0000

    Mon Apr 08 15:38:22 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -2,9 +2,9 @@
 class FizzBuzz
 
   def fizz_buzz(number)
-    if number % 2 != 0 && ((number % 15 == 0) || (number % 3 == 0 && number.to_s.include?('3')) || (number % 5 == 0 &&  number.to_s.include?('5')))
+    if (number % 2 != 0 && number % 15 == 0) || ((number % 3 == 0 && number.to_s.include?('3')) || (number % 5 == 0 &&  number.to_s.include?('5')))
       return 'fizz buzz fake deluxe'
-    elsif number % 2 == 0 && ((number % 15 == 0) || (number % 3 == 0 && number.to_s.include?('3')) || (number % 5 == 0 &&  number.to_s.include?('5')))
+    elsif (number % 2 == 0 && number % 15 == 0) || ((number % 3 == 0 && number.to_s.include?('3')) || (number % 5 == 0 &&  number.to_s.include?('5')))
       return 'fizz buzz deluxe'
     elsif number % 15 == 0 || (number % 3 == 0 && number.to_s.include?('5')) || (number % 5 == 0 && number.to_s.include?('3')) || (number.to_s.include?('3') && number.to_s.include?('5'))
       return 'fizz buzz'
@@ -30,6 +30,7 @@ class FizzBuzz
   end
 
 end
+
 
 
 
