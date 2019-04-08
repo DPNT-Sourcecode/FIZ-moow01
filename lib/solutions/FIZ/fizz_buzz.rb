5bbca056f2faeeb99385d89ec110497659b0674b
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 14:01:43 +0000

    Mon Apr 08 14:01:43 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -2,10 +2,10 @@
 class FizzBuzz
 
   def fizz_buzz(number)
-    if number > 10 && number.to_s.split('').map { |digit| digit == digit}
+    if number > 10 && (number.to_s.split('').map { |digit| digit = digit})
       return 'fizz buzz deluxe'
-    elsif number % 15 == 0 || (number % 3 == 0 && number.to_s.include?('5')) || (number % 5 == 0 && number.to_s.include?('3')) || (number.to_s.include?('3') && number.to_s.include?('5'))
-      return 'fizz buzz'
+    # elsif number % 15 == 0 || (number % 3 == 0 && number.to_s.include?('5')) || (number % 5 == 0 && number.to_s.include?('3')) || (number.to_s.include?('3') && number.to_s.include?('5'))
+    #   return 'fizz buzz'
     elsif number.to_s.include?('3') || number % 3 == 0
       return 'fizz'
     elsif number.to_s.include?('5') || number % 5 == 0
@@ -16,6 +16,7 @@ class FizzBuzz
   end
 
 end
+
 
 
 
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -6,17 +6,18 @@ describe 'Fizz Buzz' do
 end
 
 it 'returns fizz if divisible by 3' do
-  expect(FizzBuzz.new.fizz_buzz(3)).to eq('fizz')
+  expect(FizzBuzz.new.fizz_buzz(30)).to eq('fizz')
 end
 
 it 'returns buzz if divisible by 5' do
-  expect(FizzBuzz.new.fizz_buzz(5)).to eq('buzz')
+  expect(FizzBuzz.new.fizz_buzz(50)).to eq('buzz')
 end
 
 it 'returns deluxe if all digits are identical' do
   expect(FizzBuzz.new.fizz_buzz(11)).to eq('fizz buzz deluxe')
 end
 end
+
 
 
 
