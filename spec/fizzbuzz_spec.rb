9dc0d861ad0984e3d3eb7e21529e7998288ef93b
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 13:17:23 +0000

    Mon Apr 08 13:17:23 UTC 2019

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,1 +1,8 @@
-require 'lib/solutions/FIZ/fizz_buzz.rb'
+require './lib/solutions/FIZ/fizz_buzz.rb'
+
+describe 'fizz_buzz' do
+  it 'returns the number' do
+  expect(FizzBuzz.new.fizz_buzz(1)).to eq('1')
+end
+end
+
