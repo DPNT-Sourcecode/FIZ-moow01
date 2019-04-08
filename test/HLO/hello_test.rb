b9412bbe87d78689f4b105f03ebcefd59f790e26
sbx_user1051 <sbx_user1051@169.254.156.1>
2019-04-08 10:28:17 +0000

    Mon Apr 08 10:28:17 UTC 2019

---
+++ test/HLO/hello_test.rb
@@ -0,0 +1,14 @@
+require_relative '../test_helper'
+require 'logging'
+
+Logging.logger.root.appenders = Logging.appenders.stdout
+
+require_solution 'HLO'
+
+class ClientTest < Minitest::Test
+
+  def test_hello
+    assert_equal 'Hello, friend', Hello.new.hello('friend')
+  end
+
+end
