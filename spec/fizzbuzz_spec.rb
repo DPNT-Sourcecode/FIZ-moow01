f4781a402befc57270401c2b3a913f03f59bbc68
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 13:46:43 +0000

    Mon Apr 08 13:46:43 UTC 2019

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,16 @@
 require './lib/solutions/FIZ/fizz_buzz.rb'
 
-describe 'fizz_buzz' do
+describe 'Fizz Buzz' do
   it 'returns the number' do
   expect(FizzBuzz.new.fizz_buzz(4)).to eq('4')
 end
+
+it 'returns fizz if divisible by 3' do
+  expect(FizzBuzz.new.fizz_buzz(3)).to eq('fizz')
 end
+
+it 'returns buzz if divisible by 5' do
+  expect(FizzBuzz.new.fizz_buzz(5)).to eq('buzz')
+end
+end
+
