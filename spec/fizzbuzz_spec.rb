6651356e63fe9aeeda13ec19781c2df5db3cc17e
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 14:23:48 +0000

    Mon Apr 08 14:23:48 UTC 2019

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,22 +3,23 @@ require './lib/solutions/FIZ/fizz_buzz.rb'
 describe 'Fizz Buzz' do
   it 'returns the number' do
   expect(FizzBuzz.new.fizz_buzz(4)).to eq('4')
-end
+  end
 
-it 'returns fizz if divisible by 3' do
+  it 'returns fizz if divisible by 3' do
   expect(FizzBuzz.new.fizz_buzz(30)).to eq('fizz')
-end
+  end
 
-it 'returns buzz if divisible by 5' do
+  it 'returns buzz if divisible by 5' do
   expect(FizzBuzz.new.fizz_buzz(50)).to eq('buzz')
-end
+  end
 
-it 'returns deluxe if all digits are identical' do
+  it 'returns deluxe if all digits are identical' do
   expect(FizzBuzz.new.fizz_buzz(11)).to eq('deluxe')
-end
+  end
 
-  it 'returns fizz buzz delixe if all rules apply' do
-    expect(FizzBuzz.new.fizz_buzz(555)).to eq('fizz buzz deluxe')
+  it 'returns fizz buzz deluxe if all rules apply' do
+  expect(FizzBuzz.new.fizz_buzz(555)).to eq('fizz buzz deluxe')
   end
 end
+
 
